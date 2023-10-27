ef3a73b45896a879a565f0ace88924dfde9372ba
niclasbork <mr.niclas@web.de>
2023-10-27 20:35:12 +0200

    json, img, js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,70 @@
 import '../css/main.scss';
 import './helper';
-import { gsap } from "gsap";
+// import { gsap } from "gsap";
 import data from '../data/memory.json';
 
 console.log(data);
 
-for(let i = 0; i < data.length; i++) {
-    console.log(data[i]);
-    let card = document.createElement("div");
-    card.innerText = data[i].name
-    card.style.backgroundColor = data[i].color;
-    card.className = "card" + " " + data[i].class;
-    document.querySelector(".memory-wrapper").append(card);
-}
+const memoryWrapper = document.querySelector(".memory-wrapper");
+const startGame = document.querySelector(".start-game");
+let memoryCard;
 
-var tl = gsap.timeline();
+const init = function() {
+    for(let i = 0; i < data.length; i++) {
+        // console.log(data[i]);
+        let cardWrapper = document.createElement("div");
+        let card = data[i];
+        card = document.createElement("img");
+        card.src = data[i].back.src;
+        card.className = data[i].back.class + " " + data[i].class;
+        cardWrapper.className = "memory-card"
+        cardWrapper.setAttribute("data-name", data[i].name);
+        cardWrapper.append(card);
+        let cardWrapperClone = cardWrapper.cloneNode(true);
+        memoryWrapper.append(cardWrapper, cardWrapperClone);
+    }
 
-tl.fromTo("#app", { opacity: 0, x: -100 }, { opacity: 1, x: 0, duration: 1 });
+    shuffleCards();
+    flipCard();
+};
+
+const shuffleCards = function() {
+    for (let i = memoryWrapper.children.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        memoryWrapper.appendChild(memoryWrapper.children[j]);
+    }
+};
+
+// function flipCard();
+const flipCard = function() {
+    memoryCard = document.querySelectorAll(".memory-card");
+    for(let j = 0; j < memoryCard.length; j++) {
+        memoryCard[j].addEventListener("click", function() {
+            memoryCard[j].classList.add("flip");
+            console.log("Flip");
+            console.log("data-name", memoryCard[j].dataset.name);
+            console.log("has class", memoryCard[j].classList.contains("flip"));
+            checkMatch(memoryCard[j]);
+        });
+    }
+};
+
+// function checkMatch();
+const checkMatch = function(element) {
+    console.log(element);
+    if(element.dataset.name.length && element.classList.contains("flip").length) {
+        console.log("It's a match!");
+    } else {
+        console.log("Oh no!");
+    }
+    return element;
+};
+
+// function unflipCards();
+
+
+
+startGame.addEventListener("click", function() {
+    init();
+    startGame.style.display = "none";
+});
