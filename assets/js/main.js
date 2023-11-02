a7e143586aa786b108ce2076e1608be6809f1045
niclasbork <mr.niclas@web.de>
2023-11-02 18:52:54 +0100

    timing reduced

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,8 +112,8 @@ const resetCards = function(element) {
         console.log("Reset!");
         setTimeout(function() {
             element[i].classList.remove("is-open");
-        }, 1000);
-        setTimeout(shuffleCards, 1300);
+        }, 500);
+        setTimeout(shuffleCards, 800);
     }
 };
 
